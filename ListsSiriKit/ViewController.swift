989f58dff16a7b940e3ffa15d17d19f2cf4c3a0a
Martin Mitrevski <martin.mitrevski@netcetera.com>
2017-06-24 14:09:01 +0200

    Initial Commit

---
+++ ListsSiriKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ListsSiriKit
+//
+//  Created by Martin Mitrevski on 24.06.17.
+//  Copyright © 2017 Martin Mitrevski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
